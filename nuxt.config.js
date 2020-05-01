23edbfde9c6de49418acbb489c2ff6fe3f113ca2
webistomin <webistomin@gmail.com>
2020-05-01 17:25:34 +0300

    style(sass): добавил сасс переменные

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,59 @@
 module.exports = {
   mode: 'universal',
-  /*
-   ** Headers of the page
-   */
   head: {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover' },
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || '',
       },
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'format-detection', content: 'date=no' },
+      { name: 'format-detection', content: 'address=no' },
+      { name: 'format-detection', content: 'email=no' },
+      { name: 'google', content: 'notranslate' },
+      { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  /*
-   ** Customize the progress-bar color
-   */
   loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
+  css: ['~assets/sass/main'],
   plugins: [],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: ['@nuxt/typescript-build'],
-  /*
-   ** Nuxt.js modules
-   */
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots',
+    '@nuxtjs/svg-sprite',
+    '@nuxtjs/style-resources',
+  ],
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
+    '@nuxtjs/router',
   ],
+  styleResources: {
+    sass: ['./assets/sass/dev.sass'],
+  },
+  postcss: {
+    plugins: {
+      'postcss-initial': {},
+      'postcss-momentum-scrolling': ['scroll', 'auto'],
+      'postcss-object-fit-images': {},
+      'postcss-focus': {},
+      autoprefixer: {},
+    },
+  },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
   axios: {},
-  /*
-   ** Build configuration
-   */
   build: {
     /*
      ** You can extend webpack config here
