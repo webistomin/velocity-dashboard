fab1c8c9bbc815063acedbe958505b425608c369
webistomin <webistomin@gmail.com>
2020-05-06 09:12:49 +0300

    feat(video): оптимизировал видео

--- components/ui/Login/LoginVideo.tsx
+++ components/ui/Login/LoginVideo.tsx
@@ -1,16 +1,10 @@
 export const LoginVideo = () => {
   return (
     <div class='login__video-block'>
-      <video
-        preload='auto'
-        muted
-        playsinline
-        autoplay='autoplay'
-        loop='loop'
-        poster='/img/login-poster.jpg'
-        class='login__video'>
+      <video preload='auto' muted playsinline autoplay='autoplay' loop='loop' class='login__video'>
+        <source src='video/login-city.webm' type='video/webm; codecs="vp8, vorbis"' />
         <source src='video/login-city.mp4' type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
-        Тег video не поддерживается вашим браузером.
+        Video tag is not supported in this browser.
       </video>
     </div>
   );
