20e48f0fc437854ccd24612ddf9e8e8a545701fc
webistomin <webistomin@gmail.com>
2020-05-24 11:45:20 +0300

    fix(theme): поправил смену темы

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,10 +1,12 @@
 import { VueComponent } from 'types/vue-components';
 import { Component, Watch } from 'nuxt-property-decorator';
 import { VNode } from 'vue';
+import { State } from 'vuex-class';
 
 import TheHeader from 'components/partials/TheHeader';
 import TheNavigation from 'components/partials/TheNavigation';
-
+import { IUserInterfaceDB } from 'common/types/user/user-schema';
+import { SiteThemes } from 'common/types/theme/site-themes';
 import { detectUserInput } from '~/plugins/detectUserInput';
 
 @Component({
@@ -13,8 +15,19 @@ import { detectUserInput } from '~/plugins/detectUserInput';
 export default class Default extends VueComponent {
   isNavOpened: boolean = false;
 
+  @State((state) => state.auth.user)
+  getAuthUser!: IUserInterfaceDB;
+
   public mounted(): void {
     detectUserInput();
+    const localStorageTheme = localStorage.getItem('theme');
+    const userTheme = this?.getAuthUser?.theme;
+
+    if (localStorageTheme !== userTheme) {
+      const root = document.documentElement;
+      root.className = `theme theme_${userTheme}`;
+      localStorage.setItem('theme', userTheme || SiteThemes.SHELOB);
+    }
   }
 
   toggleNav() {
