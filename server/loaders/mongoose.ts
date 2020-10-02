b4c2398132f0c6cc4fb1dd46544f699a38c37bac
webistomin <webistomin@gmail.com>
2020-10-02 19:52:20 +0300

    build(*): bump deps

--- server/loaders/mongoose.ts
+++ server/loaders/mongoose.ts
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose, { Connection } from 'mongoose';
 import consola from 'consola';
-import { Db, MongoError } from 'mongodb';
+import { MongoError } from 'mongodb';
 import config from 'server/config';
 
 /**
  * Connect to MongoDB
  */
-export default async (): Promise<Db | void> => {
+export default async (): Promise<Connection | void> => {
   const url = config.mongoDB.connectString;
 
   if (url) {
@@ -14,23 +14,20 @@ export default async (): Promise<Db | void> => {
     mongoose.set('useFindAndModify', false);
     mongoose.set('useNewUrlParser', true);
 
-    const connection = await mongoose.connect(
-      url,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      (err: MongoError) => {
-        if (err) {
-          consola.error({
-            message: `Error during connection to database: ${err}`,
-            badge: true,
-          });
-        } else {
-          consola.success({
-            message: 'Successfully connected to database',
-            badge: true,
-          });
-        }
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err: MongoError) => {
+      if (err) {
+        consola.error({
+          message: `Error during connection to database: ${err}`,
+          badge: true,
+        });
+      } else {
+        consola.success({
+          message: 'Successfully connected to database',
+          badge: true,
+        });
       }
-    );
-    return connection.connection.db;
+    });
+
+    return mongoose.connection;
   }
 };
